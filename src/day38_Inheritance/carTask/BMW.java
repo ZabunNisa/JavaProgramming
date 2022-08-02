ff2831c481f0eb0ecd2374e53b6d9a361da0d3df
ZabunNisa <zebbs_nisa@hotmail.com>
2022-08-03 00:30:21 +0100

    day38: Inheritance Continue - method Override

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -1,9 +1,21 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car{
 
 
+    public BMW( String model, int year, double price, String colour, int miles) {
+        super("BMW",model, year, price, colour, miles);
+    }
 
+    public void breaksDown(){
+        System.out.println(brand+ " "+model+ " breaks Down every 100 miles");
+
+    }
+
+    public void racing(){
+        System.out.println(brand+ " "+model+ "is a racing car");
+
+    }
 
 
 }
--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -1,4 +1,20 @@
 package day38_Inheritance.carTask;
 
-public class Tesla {
+public class Tesla extends Car{
+
+
+    // Nw with const - don't need to set the intances each time:
+    public Tesla( String model, int year, double price, String colour, int miles) {
+        super("Tesla", model, year, price, colour, miles);
+    }
+
+    public void autoPilot(){
+
+        System.out.println(brand+ " "+model+ "is in autopilot mode");
+    }
 }
+
+/*3. Tesla:
+					extra methods:
+						autoPilot();
+*/
