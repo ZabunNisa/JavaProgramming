b22338a7af0a1c1d5dfa65a571ec27e04d730c63
ZabunNisa <zebbs_nisa@hotmail.com>
2022-08-03 00:19:34 +0100

    day38: Inheritance Continue - method Override

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,29 @@
+package day38_Inheritance.carTask;
+
+public class BMW {
+
+
+
+
+
+}
+/*carTask:
+		Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+
+				add a constructor to set all the fields
+
+			instance methods:
+				start(), drive(),toString()
+
+
+			2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+
+			3. Tesla:
+					extra methods:
+						autoPilot();
+*/
