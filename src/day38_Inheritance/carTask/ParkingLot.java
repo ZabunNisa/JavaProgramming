fc64d1c222375fcd7d1c451b0e569013f149cc68
ZabunNisa <zebbs_nisa@hotmail.com>
2022-08-03 11:31:25 +0100

    day38: Inheritance Continue - method Override

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+  // not parent and child class therefore no IS A relationship
+
+    //Parking Lot HAS A Toyota
+    Toyota toyota = new Toyota("Carmy",2020,20000,"Blue", 122345);
+
+    Tesla tesla = new Tesla("Model s",2003,3445,"Green",6633);
+
+    BMW bmw = new BMW("X5",2022,3333,"Black",77777);
+}
