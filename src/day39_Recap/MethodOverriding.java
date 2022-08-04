d57908b83ac5d366a597291e9171f8030eff7a17
ZabunNisa <zebbs_nisa@hotmail.com>
2022-08-04 09:45:07 +0100

    day39: Recap

---
+++ src/day39_Recap/MethodOverriding.java
@@ -0,0 +1,21 @@
+package day39_Recap;
+
+public class MethodOverriding {
+
+
+    // can use private
+
+    //method can be overloaded -same calls and pARAS same
+    public void method1(){
+
+
+    }
+
+    public int method1(int a){
+        return  a;
+
+    }
+
+
+
+}
